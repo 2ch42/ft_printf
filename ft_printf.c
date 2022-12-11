a784dc47a83fd0a4e4546f419f32577230d03b52
lee <ckdgus1120@gmail.com>
2022-12-11 19:49:28 +0900

    1

--- ft_printf.c
+++ ft_printf.c
@@ -2,5 +2,29 @@
 
 int	ft_printf(const char *str, ...)
 {
-	
+	va_list	ap;
+	int	i;
+	char	arg;
+
+	i = 0;
+	va_start(ap, str);
+	while (*(str + i))
+	{
+		if ()
+		{
+
+		}
+		else if ()
+		{
+
+		}
+		else if ()
+		{
+
+		}
+		else
+			write(1, s + i, 1);
+		i++;
+	}
+	va_end(ap);
 }
