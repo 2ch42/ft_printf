c7af58cae95b3e3985c7ef36f46bd0a5fc6727d9
Changhyeon Lee <changhyl@c10r2s1.42seoul.kr>
2022-12-12 20:29:52 +0900

    1

--- ft_printf.c
+++ ft_printf.c
@@ -1,9 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: changhyl <changhyl@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/12 20:24:38 by changhyl          #+#    #+#             */
+/*   Updated: 2022/12/12 20:24:44 by changhyl         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 
 int	ft_printf(const char *str, ...)
 {
 	va_list	ap;
-	int	i;
 	char	arg;
 
 	i = 0;
@@ -12,8 +23,7 @@ int	ft_printf(const char *str, ...)
 	{
 		if (*str == '%')
 		{
-			//
-			//va_arg ~ 
+			if 
 		}
 		else
 			write(1, s + i, 1);
